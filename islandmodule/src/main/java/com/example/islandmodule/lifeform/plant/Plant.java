985285929302906a97db3ead47324ed562c96813
User <User@DESKTOP-FUSRB4G>
2025-02-18 19:03:39 +0300

    Add Plant

---
+++ islandmodule/src/main/java/com/example/islandmodule/lifeform/plant/Plant.java
@@ -0,0 +1,8 @@
+package com.example.islandmodule.lifeform.plant;
+
+import com.example.islandmodule.lifeform.LifeForm;
+public class Plant extends LifeForm {
+    public Plant(){
+        super(1,200,"Plant");
+    }
+}
