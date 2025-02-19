b5b693bb31d2724b048541ec3d7429b9dda5c92e
User <User@DESKTOP-FUSRB4G>
2025-02-19 19:15:59 +0300

    Add

---
+++ islandmodule/src/main/java/com/example/islandmodule/lifeform/animal/herbivore/herbivore.java
@@ -0,0 +1,4 @@
+package com.example.islandmodule.lifeform.animal.herbivore;
+
+public class herbivore {
+}
---
+++ islandmodule/src/main/java/com/example/islandmodule/lifeform/animal/predator/predator.java
@@ -0,0 +1,4 @@
+package com.example.islandmodule.lifeform.animal.predator;
+
+public class predator {
+}
